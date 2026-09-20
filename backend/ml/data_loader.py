ff01104764a31514b6d0bb3dc2e5d142826392d6
himanshu140505 <himanshuyadav4596@gmail.com>
2026-09-20 22:24:18 +0530

    50 districts: 10 Nepal border states added

--- backend/ml/data_loader.py
+++ backend/ml/data_loader.py
@@ -551,6 +551,7 @@ class RealDataLoader:
             191, 310, 75, 412, 540, 620, 130, 250, 350, 480,
             399, 564, 769, 612, 338, 702, 538, 328, 748, 281,
             147, 560, 236, 447, 460, 408, 401, 172, 289, 728,
+            746, 752, 750, 755, 712, 729, 703, 665, 762, 104,
         }
         nwd = [d for d in districts if d["district_id"] in _test_ids]
         bulk = fetch_open_meteo_bulk(nwd, date_str)
